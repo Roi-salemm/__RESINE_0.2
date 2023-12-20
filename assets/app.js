3cdc69e73fa7aabd55288437657282901a240109
Agastya <sammperso@gmail.com>
2023-12-20 12:25:31 +0100

    premier commit de RESINE_0.2

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,25 @@
-import './bootstrap.js';
+// import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css'
+import './styles/app.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉')
+
+
+import './css/nav.css';
+import './css/reset.css';
+import './css/style.css';
+import './css/main/slider.css';
+import './css/main/vertical_1_slide.css';
+
+
+
+import './js/nav.js';
+import './js/main/slider.js';
+import './js/main/vertical_1_slide.js';
+
+
+// console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
